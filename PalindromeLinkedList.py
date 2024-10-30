de8d38fc2b2c6f81876d0e753d445f8076278be7
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-10-30 10:08:35 +0500

    Дан головной элемент односвязного списка. Верните true, если список является палиндромом, и false в противном случае.

---
+++ PalindromeLinkedList.py
@@ -0,0 +1,31 @@
+# Дан головной элемент односвязного списка. Верните true, если список является палиндромом, и false в противном случае.
+
+"""
+Input: head = [1,2,2,1]
+Output: true
+"""
+
+from typing import List
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        vals = []
+        currentNode = head
+
+        while currentNode is not None:
+            vals.append(currentNode.val)
+            currentNode = currentNode.next
+
+        front = 0
+        back = len(vals) - 1
+        while front < back:
+            if vals[front] != vals[back]:
+                return False
+            front += 1
+            back -= 1
+        return True
+
+if __name__ == "__main__":
+    example = Solution()
+    res = example.isPalindrome(head = [1,2,2,1])
+    print(res)

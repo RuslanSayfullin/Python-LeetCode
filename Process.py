6ab76884ac956cc900bd765b2bf9cd3c4fb02903
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-03-18 09:02:15 +0500

    Process — это класс для создания и управления отдельными процессами

---
+++ Process.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python3
+from multiprocessing import Process
+import os
+import time
+
+# Process — это класс для создания и управления отдельными процессами 
+def worker():
+    print(f"Worker process with PID: {os.getpid()}")
+    time.sleep(2)
+    print("Worker process ended")
+
+if __name__ == "__main__":
+    print(f"Main process PID: {os.getpid()}")
+
+    p = Process(target=worker)
+    p.start()
+    print("Worker process started")
+
+    p.join()
+    print("Worker process joined")
+
+    print("Main process ended")
